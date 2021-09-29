9732102cefe053b35a461a09a15b24cf07191b45
Isaiah Skelton <22wiskelton@macon.k12.mo.us>
2021-09-29 13:20:56 -0500

    Learning about time spans

---
+++ Time_Spans_Assignment/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Time_Spans_Assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, RoutedEventArgs e)
+        {
+            // http://is.gd/timespan
+            //Days.Hours:Minutes:Seconds.Milliseconds
+
+            TimeSpan myTimeSpan = TimeSpan.Parse("1.6:35:22.6");
+
+            DateTime myBirthday = DateTime.Parse("10/09/2003");
+
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+            //labelResult.Content = myAge.Hours.ToString();
+
+            labelResult.Content = myAge.TotalDays.ToString();
+
+        }
+    }
+}
